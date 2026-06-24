890bdce8ffb70da1f3e23894be108805a25c5209
JohNan <JohNan@users.noreply.github.com>
2026-06-24 18:52:48 +0000

    chore: update card resource cache buster to latest visualizer commit hash d8b17f3

--- custom_components/weatherflow_lightning_trilateration/__init__.py
+++ custom_components/weatherflow_lightning_trilateration/__init__.py
@@ -1058,7 +1058,7 @@ async def _async_register_lovelace_resource(hass: HomeAssistant) -> None:
             await resources.async_load()
 
     base_url = "/weatherflow_lightning_trilateration/weatherflow-lightning-card.js"
-    url = f"{base_url}?v=356f1e1"
+    url = f"{base_url}?v=d8b17f3"
 
     existing_item = None
     if hasattr(resources, "async_items"):
