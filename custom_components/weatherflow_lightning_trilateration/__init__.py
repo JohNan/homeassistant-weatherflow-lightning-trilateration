6a9ce655c87eb926b22628d8f7cb9b9e02b49fc5
JohNan <JohNan@users.noreply.github.com>
2026-06-24 21:02:54 +0000

    Update Lovelace card resource cache buster to 885e775

--- custom_components/weatherflow_lightning_trilateration/__init__.py
+++ custom_components/weatherflow_lightning_trilateration/__init__.py
@@ -1058,7 +1058,7 @@ async def _async_register_lovelace_resource(hass: HomeAssistant) -> None:
             await resources.async_load()
 
     base_url = "/weatherflow_lightning_trilateration/weatherflow-lightning-card.js"
-    url = f"{base_url}?v=f30df0e"
+    url = f"{base_url}?v=885e775"
 
     existing_item = None
     if hasattr(resources, "async_items"):
